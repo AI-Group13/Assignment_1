5d9571b47c175a4ec6cef35fd1136199171c0880
Dominic Cupo <Dominic.Cupo@gmail.com>
2018-01-30 19:54:02 -0500

    Added helpers for generating empty boards of proper size

--- Urban_Planning/urban_planner_helpers.py
+++ Urban_Planning/urban_planner_helpers.py
@@ -25,3 +25,28 @@ def read_input_file(path_to_input_file):
         board.append(list(line_list[ii]))
 
     return num_industrial, num_commercial, num_residential, board
+
+
+def generate_empty_board(x, y):
+    board = []
+
+    for x in range(0, x):
+
+        y_vector = []
+
+        for y in range(0, y):
+            y_vector.append(' ')
+
+        board.append(y_vector)
+
+    return board
+
+
+def generate_starting_boards(number_to_make, x, y):
+    list_of_starting_boards = []
+
+    for ii in range(number_to_make):
+        list_of_starting_boards.append(generate_empty_board(x, y))
+
+    return list_of_starting_boards
+
